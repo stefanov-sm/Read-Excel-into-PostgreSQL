99ce3803f405e38ab5e005597e0ff2b3e25056c2
Stefan Stefanov <stefanov.sm@abv.bg>
2022-10-11 14:57:10 +0300

    Add files via upload

---
+++ functions.sql
@@ -0,0 +1,52 @@
+CREATE OR REPLACE FUNCTION spreadsheetml_dump(xl xml)
+RETURNS TABLE(row_number integer, cell_data text, cell_type text) LANGUAGE sql as
+$function$
+ select * 
+ from xmltable
+ (
+  xmlnamespaces
+  (
+   'urn:schemas-microsoft-com:office:spreadsheet' as msoxl,
+   'urn:schemas-microsoft-com:office:office'      as o,
+   'urn:schemas-microsoft-com:office:excel'       as x,
+   'urn:schemas-microsoft-com:office:spreadsheet' as ss,
+   'http://www.w3.org/TR/REC-html40' as html
+  ), 
+  '/msoxl:Workbook/msoxl:Worksheet/msoxl:Table/msoxl:Row/msoxl:Cell' 
+  passing by ref xl
+  columns 
+  row_number for ordinality, 
+  cell_data text path 'msoxl:Data', 
+  cell_type text path 'msoxl:Data/@msoxl:Type'
+ );
+$function$;
+
+CREATE OR REPLACE FUNCTION spreadsheetml_fromfile(xlfile text)
+RETURNS TABLE(row_number integer, cell_data text, cell_type text) LANGUAGE plpgsql as
+$function$
+declare
+ lo_oid oid;
+ xlxml xml;
+begin
+ lo_oid := lo_import(xlfile);
+ xlxml := convert_from(lo_get(lo_oid), 'utf-8')::xml;
+ perform lo_unlink(lo_oid);
+ return query select * from xmltable
+ (
+  xmlnamespaces
+  (
+   'urn:schemas-microsoft-com:office:spreadsheet' as msoxl,
+   'urn:schemas-microsoft-com:office:office'      as o,
+   'urn:schemas-microsoft-com:office:excel'       as x,
+   'urn:schemas-microsoft-com:office:spreadsheet' as ss,
+   'http://www.w3.org/TR/REC-html40' as html
+  ), 
+  '/msoxl:Workbook/msoxl:Worksheet/msoxl:Table/msoxl:Row/msoxl:Cell' 
+  passing by ref xlxml
+  columns
+  row_number for ordinality, 
+  cell_data text path 'msoxl:Data', 
+  cell_type text path 'msoxl:Data/@msoxl:Type'
+ );
+end;
+$function$;
---
+++ usage.sql
@@ -0,0 +1,10 @@
+select 
+ (row_number - 1) / 5 ordinal,
+ max(cell_data) filter (where row_number % 5 =  1) "A",  
+ max(cell_data) filter (where row_number % 5 =  2) "B",  
+ max(cell_data) filter (where row_number % 5 =  3) "C",
+ max(cell_data) filter (where row_number % 5 =  4) "D",  
+ max(cell_data) filter (where row_number % 5 =  0) "E"
+from spreadsheetml_fromfile('path-to/ecb.xml') 
+group by ordinal
+order by ordinal;
