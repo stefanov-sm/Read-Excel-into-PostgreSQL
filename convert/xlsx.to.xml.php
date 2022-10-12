040e0737c7b288dafa40ef65e1cd6aab58e540bf
Stefan Stefanov <stefanov.sm@abv.bg>
2022-10-12 11:49:40 +0300

    PHP conversion function

--- convert/xlsx.to.xml.php
+++ convert/xlsx.to.xml.php
@@ -13,15 +13,3 @@ function xlsx_to_spreadsheetml($xlsx_filename, $xml_filename)
     $workbook -> Close(FALSE);
     $excel_engine -> Quit();
 };
-
-function spreadsheetml_to_xlsx($report_filename, $excel_filename)
-{
-    $excel_engine = new COM('Excel.Application');
-    $excel_engine -> Visible = FALSE;
-    $excel_engine -> DisplayAlerts = FALSE;
-
-    $workbook = $excel_engine -> Workbooks -> Open($report_filename);
-    $workbook -> SaveAs($excel_filename, xlOpenXMLWorkbook);
-    $workbook -> Close(FALSE);
-    $excel_engine -> Quit();
-};
