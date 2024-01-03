93b1d8389fd7ed63ecb37e1fcca75a340af073ea
Stefan Stefanov <stefanov.sm@abv.bg>
2024-01-03 16:40:24 +0200

    Update usage.sql

--- usage.sql
+++ usage.sql
@@ -1,12 +1,12 @@
 -- 5 is a parameter, the number of spreadsheet columns
-with t as 
+with t as
 (
  select *,
  (rn - 1) % 5 col_no, -- rn is 1-based
  (rn - 1) / 5 row_no
  from spreadsheetml_fromfile('path-to/ecb.xml')
 )
-select 
+select
   max(cell_data) filter (where col_no = 0) subject,
   max(cell_data) filter (where col_no = 1) "name",
   max(cell_data) filter (where col_no = 2) ::timestamp date_time,
@@ -14,4 +14,5 @@ select
   max(cell_data) filter (where col_no = 4) ::numeric rate
 from t
 where row_no > 0 -- skip the title row
-group by row_no order by row_no;
+group by row_no
+order by row_no;
